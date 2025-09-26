347d64f3860dd3bc2d93681fdbd0cdd4e63728b5
Shrek-hubs <shrek@dir.bg>
2025-09-26 19:57:13 +0300

    Update M2-automate-iis.ps1

--- M2-automate-iis.ps1
+++ M2-automate-iis.ps1
@@ -1,7 +1,10 @@
-try {
-    Install-WindowsFeature -name Web-Server -IncludeManagementTools -ErrorAction Stop
-    $hostname = hostname
-    Set-Content -Path "C:\inetpub\wwwroot\index.html" -Value "<html><body><h1>Hello from $hostname</h1></body></html>"
-} catch {
-    Write-Output "Error: $_"
-}
+Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+
+$hostname = hostname
+$customHtml = @"
+<html>
+  <head><title>Hello from $hostname</title></head>
+  <body><h1>Hello from $hostname</h1></body>
+</html>
+"@
+Set-Content -Path "C:\inetpub\wwwroot\index.html" -Value $customHtml
